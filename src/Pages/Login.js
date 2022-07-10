3ff869aa5c738b51a26bd862696bc19340e20b8d
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-07-10 14:02:33 -0300

    Adiciona requisito 2

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,19 +1,63 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: '',
+      isActive: true,
+      loadingScreen: false,
+      loaded: false,
+    };
+  }
+
+  saveUser = (event) => {
+    const min = 3;
+    if (event.target.value.length >= min) {
+      this.setState({
+        isActive: false,
+        userName: event.target.value,
+      });
+    }
+  }
+
+  login = async (event) => {
+    event.preventDefault();
+    const { userName } = this.state;
+    this.setState({ loadingScreen: true });
+    await createUser({ name: userName });
+    this.setState({ loadingScreen: false, loaded: true });
+  }
+
   render() {
+    const { isActive, loadingScreen, loaded } = this.state;
     return (
       <section data-testid="page-login">
+
+        {loadingScreen && <Loading />}
+        {loaded && <Redirect to="/search" />}
         <form>
           <input
             type="text"
             name="input-name"
+            data-testid="login-name-input"
             placeholder="Digite seu Login"
+            onChange={ this.saveUser }
           />
           <button
+            name="login-btn"
             type="submit"
+            data-testid="login-submit-button"
+            disabled={ isActive }
+            onClick={ this.login }
           >
-            Login
+
+            Entrar
+
           </button>
 
         </form>
