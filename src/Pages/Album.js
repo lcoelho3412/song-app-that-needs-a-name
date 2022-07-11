590b859dca6113b338bb23bfa8bd6fd2cef5bec1
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-07-11 16:34:02 -0300

    Adciona requisito 7

--- src/Pages/Album.js
+++ src/Pages/Album.js
@@ -1,17 +1,70 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../Components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      music: [],
+      artist: '',
+      collection: '',
+      image: '',
+    };
+  }
+
+  componentDidMount() {
+    this.requisition();
+  }
+
+  requisition = async () => {
+    const { match: { params: { id } } } = this.props;
+    const artistSong = await getMusics(id);
+    console.log(artistSong);
+    this.setState({
+      artist: artistSong[0].artistName,
+      collection: artistSong[0].collectionName,
+      music: artistSong.filter((item) => item.kind === 'song'),
+      image: artistSong[0].artworkUrl100,
+    });
+  }
+
   render() {
+    const { artist, collection, music, image } = this.state;
+    console.log({ music });
     return (
       <>
         <Header />
         <section data-testid="page-album">
-          <p>Album</p>
+          <br />
+          <img src={ image } alt="imagem do album que esta tocando." />
+          <br />
+          <h2 data-testid="artist-name">{artist}</h2>
+          <h4 data-testid="album-name">{collection}</h4>
+          {music.map((song) => (
+            <div
+              key={ song.trackId }
+              music={ song }
+              data-testid="audio-component"
+            >
+              <p>{song.artist}</p>
+              <ul>
+                <li>
+                  { song.trackName }
+                </li>
+              </ul>
+            </div>
+
+          ))}
         </section>
       </>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.string,
+}.isRequired;
 
 export default Album;
